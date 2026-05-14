bbf2f8fba9de52bfce0475b5811a8acde2dcefc0
ratnadeep2003 <ratnadeepabitkar@gmail.com>
2026-05-14 12:20:47 +0530

    fix: resolved prisma accelerate CF workers setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,34 @@
 import { Hono } from 'hono';
-import { PrismaClient } from './generated/prisma/client'
+import { PrismaClient } from './generated/prisma/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { sign } from 'hono/utils/jwt/jwt';
 
 type Bindings = {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string,
 }
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.post('/api/v1/signup', async (c) => {
-    const adapter = new PrismaPg({ connectionString: c.env.DATABASE_URL })
-    const prisma = new PrismaClient({ adapter }).$extends(withAccelerate())
-    return c.text('signup route')
+    try {
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL,
+        }).$extends(withAccelerate())
+        
+        const body = await c.req.json();
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password: body.password,
+            },
+        })
+        
+        const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+        return c.json({ jwt: token })
+    } catch(e) {
+        return c.json({ error: String(e) }, 500)
+    }
 })
 
 export default app;
